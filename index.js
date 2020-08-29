ebbb47aa8fa569fc7d3f9675cf070319c4f2ac49
Inixio <inixio97@gmail.com>
2020-08-29 16:08:10 +0200

    Persistency added

--- index.js
+++ index.js
@@ -1,29 +1,63 @@
 const Telegraf = require('telegraf');
 const cron = require("node-cron");
 const axios = require("axios");
+const mongoose = require("mongoose")
 const dotenv = require('dotenv');
 dotenv.config();
+mongoose.connect(`mongodb://${process.env.MONGO_URL}/telegramastrobot`, {useNewUrlParser: true});
 
-let data = {};
-
-cron.schedule("1 0 * * *", function() {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then(res => {
-        data = res.data;
-    })
+const ChatSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        unique: true
+    },
+    username: String
 });
 
-axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then(res => {
-    data = res.data;
-    const bot = new Telegraf(process.env.BOT_TOKEN)
-    bot.start((ctx) => {
-        ctx.reply("Welcome! I will send you the Astronomy Picture of the Day, along with a brief explanation written by a professional astronomer.\n\nI send the picture everyday at 15:00 AM (GMT +1)\n\nYou can get the picture whenever you want by typing /get")
-        cron.schedule("00 15 * * *", function() {
+const Chat = mongoose.model('Chat', ChatSchema);
+
+let data = {};
+const db = mongoose.connection;
+db.once('open', function() {
+    cron.schedule("1 0 * * *", function() {
+        axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then(res => {
+            data = res.data;
+        })
+    });
+
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then(res => {
+        data = res.data;
+        const bot = new Telegraf(process.env.BOT_TOKEN)
+        bot.start((ctx) => {
+            ctx.getChat().then(chat => {
+                const {id, username} = chat;
+                const subscriber = new Chat({id, username});
+                subscriber.save().catch(e => console.log("Duplicated user"));
+            });
+            ctx.reply("Welcome! I will send you the Astronomy Picture of the Day, along with a brief explanation written by a professional astronomer.\n\nI send the picture everyday at 12:00 AM (GMT +1)\n\nYou can get the picture whenever you want by typing /get\n\nIf I don't respond, type /start")
+        })
+        bot.command('get', (ctx) => {
             ctx.replyWithPhoto(data.hdurl, {caption: `${data.title}\n\n${data.explanation}`})
         });
-    })
-    bot.command('get', (ctx) => {
-        ctx.replyWithPhoto(data.hdurl, {caption: `${data.title}\n\n${data.explanation}`})
-    });
-    bot.launch()
-})
+        bot.command('stop', (ctx) => {
+            ctx.getChat().then(chat => {
+                Chat.deleteOne({id: chat.id}, (err) => {
+                    ctx.reply("Okay, I'll stop sending you the Astronomy Picture of the day")
+                })
+            })
+        })
+        cron.schedule("0 12 * * *", () => {
+            const telegram = new Telegraf.Telegram(process.env.BOT_TOKEN);
+            Chat.find({}, (err, chats) => {
+                console.log(`${chats.length} active users`)
+                if (!err) {
+                    chats.map(chat => {
+                        telegram.sendPhoto(chat.id, data.hdurl, {caption: `${data.title}\n\n${data.explanation}`})              
+                    })
+                }
+            })
+        })
+        bot.launch()
+    })    
+});
 
